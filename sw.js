89ab33d13ce19059ac3bb69a45d91341762367b1
tvavrda <tvavrda@gmail.com>
2026-06-10 08:39:22 +0200

    Tighten content-only preview layout

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = "adaptivio-cms-shell-v41";
+const CACHE_NAME = "adaptivio-cms-shell-v42";
 const SHELL_ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./assets/styles.css?v=20260610-143600",
-  "./assets/favicon.svg?v=20260610-143600",
-  "./src/app.js?v=20260610-143600",
-  "./src/editorWorkflow.js?v=20260610-143600",
-  "./src/github.js?v=20260610-143600",
-  "./src/i18n.js?v=20260610-143600",
-  "./src/repoCache.js?v=20260610-143600",
-  "./src/storage.js?v=20260610-143600",
-  "./src/utils.js?v=20260610-143600",
+  "./assets/styles.css?v=20260610-144300",
+  "./assets/favicon.svg?v=20260610-144300",
+  "./src/app.js?v=20260610-144300",
+  "./src/editorWorkflow.js?v=20260610-144300",
+  "./src/github.js?v=20260610-144300",
+  "./src/i18n.js?v=20260610-144300",
+  "./src/repoCache.js?v=20260610-144300",
+  "./src/storage.js?v=20260610-144300",
+  "./src/utils.js?v=20260610-144300",
   "./cms.config.json",
 ];
 
